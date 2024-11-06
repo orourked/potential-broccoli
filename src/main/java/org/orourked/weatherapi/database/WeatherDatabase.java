490d01843d05aa320e8b98fa603a6f938d2bab34
Darren O'Rourke <darren.orourke@gmail.com>
2024-11-06 16:42:24 +0000

    Restructured project, setting up connection to DB.

--- src/main/java/org/orourked/weatherapi/database/WeatherDatabase.java
+++ src/main/java/org/orourked/weatherapi/database/WeatherDatabase.java
@@ -8,5 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WeatherDatabase extends MongoRepository<WeatherData, String> {
   List<WeatherData> findByLocation(String location);
-  //List<WeatherData> findAll();
 }
